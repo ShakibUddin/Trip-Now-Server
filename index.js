3c63b124de41efca3f7e122622c2eced52e639bd
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-10-29 00:53:21 +0600

    added trips get route

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+//database url
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@main.vzl7z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const port = process.env.PORT || 5000;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db(process.env.DB_NAME);
+        const tripsCollection = database.collection('trips');
+
+        // GET API
+        app.get('/trips', async (req, res) => {
+            const cursor = tripsCollection.find({});
+            const trips = await cursor.toArray();
+            res.send(trips);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log("listening to", port);
+});
